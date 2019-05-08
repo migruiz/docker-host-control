58b350a188a38c85271939050d66792002210af3
miguel <mig.ruiz@gmail.com>
2019-05-08 13:36:08 +0100

    detached script

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-sudo docker run -it --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" --pid host --privileged=true migruiz/docker-host-control
+sudo xhost +
+sudo docker run -d --name=host-control --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" --pid host --privileged=true --restart=always migruiz/docker-host-control
