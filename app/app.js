93c74863421c9bffb81f59655df51a732370e4c7
miguel <mig.ruiz@gmail.com>
2019-05-08 12:07:04 +0100

    only logging 3 lcicks

--- app/app.js
+++ app/app.js
@@ -4,7 +4,9 @@ const ioHook = require('iohook');
 
 
 ioHook.on('mousedown', event => {
-  console.log(event); // { type: 'mousemove', x: 700, y: 400 }
+  if (event.clicks>2){
+    console.log(event); // { type: 'mousemove', x: 700, y: 400 }
+  }
 });
 
 // Register and start hook
