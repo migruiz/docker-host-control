06678d028b694a39f527f97a4376e750713d5dd4
miguel <mig.ruiz@gmail.com>
2019-05-08 12:45:53 +0100

    checking for teamviewer and killing it

--- app/app.js
+++ app/app.js
@@ -6,6 +6,20 @@ const ioHook = require('iohook');
 ioHook.on('mousedown', event => {
   if (event.clicks>2){
     console.log(event); // { type: 'mousemove', x: 700, y: 400 }
+const spawn = require('child_process').spawn;
+const ls = spawn('killall', ['-q','-I', 'teamviewer']);
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
   }
 });
 
